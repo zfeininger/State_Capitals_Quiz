ad2680d25a8088ced6fb35b61de86fee848117ef
zacharyfeininger <zaf83063@uga.edu>
2023-11-02 15:07:05 -0400

    Restructuring for csv read

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.uga.cs.statecapitalsquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment fragment = new readcsv();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace( R.id.fragmentContainerView, fragment).addToBackStack("main screen").commit();
     }
 }
