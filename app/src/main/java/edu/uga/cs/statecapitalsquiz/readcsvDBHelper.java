2ed97bba6ea8ed0fb7c69e7daeda76fc1cb1e937
zacharyfeininger <zaf83063@uga.edu>
2023-11-02 15:34:53 -0400

    New DBHelper for table 1 (states)

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/readcsvDBHelper.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/readcsvDBHelper.java
@@ -2,13 +2,54 @@ package edu.uga.cs.statecapitalsquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.util.Log;
 
-public class readcsvDBHelper extends AppCompatActivity {
+public class readcsvDBHelper extends SQLiteOpenHelper {
+
+    private static final String DEBUG_TAG = "readcsvDBHelper";
+    private static final String DB_Name = "jobsleads.db";
+    private static final int DB_VERSION = 1;
+    public static final String TABLE_STATES = "states";
+    public static final String STATES_COLUMN_ID = "_id";
+    public static final String STATES_COLUMN_STATES = "state";
+    public static final String STATES_COLUMN_CAPITALCITY = "capitalCity";
+    public static final String STATES_COLUMN_ADDITIONALCITY1 = "additionalCity1";
+    public static final String STATES_COLUMN_ADDITIONALCITY2 = "additionalCity2";
+    private static readcsvDBHelper helperInstance;
+
+    private static final String CREATE_STATES =
+            "create table " + TABLE_STATES + " ("
+            + STATES_COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + STATES_COLUMN_STATES + " TEXT, "
+            + STATES_COLUMN_CAPITALCITY + " TEXT, "
+            + STATES_COLUMN_ADDITIONALCITY1 + " TEXT, "
+            + STATES_COLUMN_ADDITIONALCITY2 + " TEXT"
+            + ")";
+
+    private readcsvDBHelper (Context context ) {super (context, DB_Name, null, DB_VERSION);}
+
+    public static synchronized  readcsvDBHelper getInstance(Context context) {
+        if ( helperInstance == null) {
+            helperInstance = new readcsvDBHelper( context.getApplicationContext());
+        }
+        return helperInstance;
+    }
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_readcsv_dbhelper);
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL (CREATE_STATES);
+        Log.d(DEBUG_TAG, "Table " + TABLE_STATES + " created");
+    }
+
+    @Override
+    public void onUpgrade ( SQLiteDatabase db, int oldVersion, int newVersion ) {
+        db.execSQL( "drop table if exists " + TABLE_STATES);
+        onCreate(db);
+        Log.d( DEBUG_TAG, "Table " + TABLE_STATES + " upgraded" );
     }
 }
