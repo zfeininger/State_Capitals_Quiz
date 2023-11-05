95a49de8706c462b99ce86f05f08d29fb91dba80
zacharyfeininger <zaf83063@uga.edu>
2023-11-05 16:46:32 -0500

    beginning reading data into start quiz, needs further work

--- app/src/main/java/edu/uga/cs/statecapitalsquiz/StartQuizFragment.java
+++ app/src/main/java/edu/uga/cs/statecapitalsquiz/StartQuizFragment.java
@@ -4,9 +4,13 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,50 +19,67 @@ import android.view.ViewGroup;
  */
 public class StartQuizFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TextView textview;
+    private readcsvData readcsvData = null;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public StartQuizFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment StartQuizFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static StartQuizFragment newInstance(String param1, String param2) {
         StartQuizFragment fragment = new StartQuizFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start_quiz, container, false);
+        readcsvData = new readcsvData( getActivity() );
+        if (readcsvData != null)
+            readcsvData.open();
+        View view = inflater.inflate(R.layout.fragment_start_quiz, container, false);
+        textview = view.findViewById(R.id.textview1);
+        List<readcsv> readcsvList = new readcsvDBReader().doInBackground();
+        for (readcsv item : readcsvList) {
+            Log.d("JUST TO CHECK SOON TO DELETE", "test: " + item.toString());
+        }
+        return view;
+    }
+
+    public class readcsvDBReader extends AsyncTask<Void, List<readcsv>> {
+        @Override
+        protected List<readcsv> doInBackground(Void... params) {
+            List<readcsv> readcsvList = readcsvData.retrieveAllreadcsvLeads();
+            Log.d("TAG", "States Recieved: " + readcsvList.size());
+            return readcsvList;
+        }
+        @Override
+        protected void onPostExecute(List<readcsv> readcsv) {
+            Log.d("Tag", "Job List size");
+//            readcsvList.addAll(readcsvList);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (readcsvData != null && !readcsvData.isDBOpen())
+            readcsvData.open();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (readcsvData != null && readcsvData.isDBOpen()) {
+            readcsvData.close();
+        }
     }
 }
